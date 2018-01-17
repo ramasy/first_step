d578b666f5149c917c26698bc86bb35281b5797e
RAMASY Heritier . A <heritier.ramasy@gmail.com>
2018-01-17 11:38:28 +0300

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'tests#index'
+  devise_for :users
   get 'tests/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
